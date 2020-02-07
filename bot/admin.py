b7b722e33b03f4fa4118ab21fa6560e8ee17424f
ekhoshnood <e.khoshnood966@gmail.com>
2020-02-07 22:30:57 +0330

    start setting models

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Senf, SaleType, Channel, Post, MyModelAdmin
 
-# Register your models here.
+
+admin.site.register(Senf)
+admin.site.register(SaleType)
+admin.site.register(Channel, MyModelAdmin)
+admin.site.register(Post)
