4bb32c479cd6b16146b11d45c3a85422a61a2540
ekhoshnood <e.khoshnood966@gmail.com>
2020-02-09 19:53:01 +0330

    register page

--- account/forms.py
+++ account/forms.py
@@ -5,6 +5,21 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+    username = forms
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "نام کاربری خود را تایپ کنید",
+                                                             'class': 'input100',
+                                                             }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'ایمیل',
+                                                           'class': 'input100',
+                                                           }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'رمز خود را وارد کنید',
+                                                                  'class': 'input100',
+                                                                  }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'رمز خود را مجددا وارد کنید',
+                                                                  'class': 'input100',
+                                                                  }))
